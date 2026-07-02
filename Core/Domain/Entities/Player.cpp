2eca1a2c849adc0a93c6d422e2a8d23089d25c07
FatemehNajafpour <fatemehnajafpour27@gmail.com>
2026-07-03 02:43:49 +0330

    feat(domain): add player entity

---
+++ Core/Domain/Entities/Player.cpp
@@ -0,0 +1,31 @@
+#include "Player.h"
+Player::Player(std::int64_t id):p_ID_(id),p_score_(0),p_isReady_(false){
+
+}
+const std::int64_t Player::pID() const noexcept {
+	return p_ID_;
+}
+int Player::pScore() const noexcept {
+	return p_score_;
+}
+bool Player::pIsReady() const noexcept {
+	return p_isReady_;
+}
+void Player::ready() {
+	p_isReady_ = true;
+}
+void Player::unready() {
+	p_isReady_ = false;
+}
+void Player::addScore(int s) {
+	p_score_ += s;
+}
+void Player::decreaseScore(int s) {
+	p_score_ -= s;
+}
+void Player::resetScore() {
+	p_score_ = 0;
+}
+bool Player::operator==(const Player& player) const {
+	return player.pID() == p_ID_;
+}
---
+++ Core/Domain/Entities/Player.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <cstdint>
+class Player
+{
+private:
+	std::int64_t p_ID_; //equal with u_ID_
+	int p_score_;
+	bool p_isReady_;
+	
+public:
+	Player(std::int64_t id);
+	const std::int64_t pID() const noexcept;
+	int pScore() const noexcept;
+	bool pIsReady() const noexcept;
+	void ready();
+	void unready();
+	void addScore(int);
+	void decreaseScore(int);
+	void resetScore();
+	bool operator==(const Player&) const;
+
+};
