97c4f40af8f39262b8f33c0baaaab8114737d6a3
Mohsen_Hatami <mohsenhatami13852006@gmail.com>
2026-07-06 00:43:29 +0330

    fix: improve session context

--- Core/Domain/Entities/authenticateduser.cpp
+++ Core/Domain/Entities/authenticateduser.cpp
@@ -1,28 +1,29 @@
 #include "authenticateduser.h"
 
+
 AuthenticatedUser::AuthenticatedUser(std::int64_t id , const Name& name, const UserName& username):u_ID_(id),u_name_(name),u_username_(username){
 
 }
-std::int64_t AuthenticatedUser::ID() const noexcept{
-    return ID_;
+std::int64_t AuthenticatedUser::id() const noexcept{
+    return u_ID_;
 }
-const Name& AuthenticatedUser::Name() const noexcept {
-    return name_;
+const Name& AuthenticatedUser::name() const noexcept {
+    return u_name_;
 }
-const UserName& AuthenticatedUser::UserName() const noexcept {
-    return username_;
+const UserName& AuthenticatedUser::username() const noexcept {
+    return u_username_;
 }
 bool AuthenticatedUser::changeName(const Name& name) {
-    if (name == name_) {
+    if (name == u_name_) {
         return false;
     }
-    name_ = name;
+    u_name_ = name;
     return true;
 }
 bool AuthenticatedUser::changeUserName(const UserName& username) {
-    if (username == username_) {
+    if (username == u_username_) {
         return false;
     }
-    username_ = username;
+    u_username_ = username;
     return true;
 }
