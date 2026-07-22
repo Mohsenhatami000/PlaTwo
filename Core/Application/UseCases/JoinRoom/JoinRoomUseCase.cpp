604499f7fa3eabcfbdfabddc7f94a7ff40b32f09
FatemehNajafpour <fatemehnajafpour27@gmail.com>
2026-07-23 00:54:25 +0330

    fix(client):  send Auth packet before JoinRoom

--- Core/Application/UseCases/JoinRoom/JoinRoomUseCase.cpp
+++ Core/Application/UseCases/JoinRoom/JoinRoomUseCase.cpp
@@ -2,11 +2,18 @@
 #include "../../../../Infrastructure/Network/Packet/joinroompacket.h"
 
 
-JoinRoomUseCase::JoinRoomUseCase(IJoinRoomPresenter* pre, INetworkClient* net) :presenter_(pre), network_(net) {
+
+JoinRoomUseCase::JoinRoomUseCase(IJoinRoomPresenter* pre, INetworkClient* net, ISessionContext* session) :presenter_(pre),
+                  network_(net),session_(session) {
 
 }
 
 void JoinRoomUseCase::execute(std::string ip , std::int64_t port, std::int64_t roomID){
+    network_->Connect(ip, port);
+    std::optional<AuthenticatedUser> user = session_->currentUser();
+
+    AuthenticatePacket authPacket(user.value().id(), user.value().name().nameValue(), user.value().username().userNameValue());
+    network_->send(authPacket);
 
     JoinRoomPacket packet(ip, port, roomID);
     network_->send(packet);
--- Core/Application/UseCases/JoinRoom/JoinRoomUseCase.h
+++ Core/Application/UseCases/JoinRoom/JoinRoomUseCase.h
@@ -2,16 +2,19 @@
 #define JOINROOMUSECASE_H
 #include "Domain/Interfaces/Services/IJoinRoomPresenter.h"
 #include "Domain/Interfaces/Services/inetworkclient.h"
-
+#include "../../../../Infrastructure/Network/Packet/authenticatepacket.h"
+#include <Domain/Entities/authenticateduser.h>
+#include "Domain/Interfaces/Services/isessioncontext.h"
 
 class JoinRoomUseCase
 {
 private:
 	IJoinRoomPresenter* presenter_;
 	INetworkClient* network_;
+	ISessionContext* session_;
 
 public:
-	JoinRoomUseCase(IJoinRoomPresenter* pre, INetworkClient* net);
+	JoinRoomUseCase(IJoinRoomPresenter* pre, INetworkClient* net, ISessionContext* session);
     void execute(std::string ip , std::int64_t port, std::int64_t roomID);
 
 };
